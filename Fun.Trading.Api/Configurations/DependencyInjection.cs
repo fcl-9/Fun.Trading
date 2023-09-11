a674798bfeca5d1a83fe75d631a158111967d8d4
fcl-93 <fabio_cl93_@hotmail.com>
2023-09-11 21:34:26 +0100

    Dummy Auth -To Be Replaced By SPA

---
+++ Fun.Trading.Api/Configurations/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Fun.Trading.Api.DependencyInjectionConfigurations
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureAuthServices(this IServiceCollection services, IConfiguration configuration) {
+            var authOptions = new AuthOptions();
+            configuration.GetSection(AuthOptions.Auth).Bind(authOptions);
+            
+            services.Configure<AuthOptions>(configuration.GetSection(AuthOptions.Auth));
+
+            services.AddScoped<IAuthenticationApiClient>(svc => new AuthenticationApiClient(authOptions.Domain));
+        }
+    }
+}
--- Fun.Trading.Api/Controllers/UserManagementController.cs
+++ Fun.Trading.Api/Controllers/UserManagementController.cs
@@ -1,6 +1,10 @@
-﻿using Fun.Trading.Api.Model;
+﻿using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Fun.Trading.Api.DependencyInjectionConfigurations;
+using Fun.Trading.Api.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Fun.Trading.Api.Controllers
 {
@@ -9,6 +13,15 @@ namespace Fun.Trading.Api.Controllers
     [ApiController]
     public class UserManagementController : ControllerBase
     {
+        private readonly IAuthenticationApiClient _authClient;
+        private readonly IOptions<AuthOptions> _authOptions;
+
+        public UserManagementController(IAuthenticationApiClient authClient, IOptions<AuthOptions> authOptions)
+        {
+            _authClient = authClient;
+            _authOptions = authOptions;
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -36,26 +49,50 @@ namespace Fun.Trading.Api.Controllers
 
         /// <summary>
         /// Registers a new user
+        /// Temporary to be replaced by SPA
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
         [HttpPost("register")]
-        public IActionResult RegisterUser(RegisterRequest request)
+        public async Task<IActionResult> RegisterUser(RegisterRequest request)
         {
-            throw new NotImplementedException();
+            var userRequest = new SignupUserRequest()
+            {
+                ClientId = _authOptions.Value.ClientId,
+                Email = request.Email,
+                Password = request.Password,
+                Connection = _authOptions.Value.Connection
+            };
+
+            var authUser = await _authClient.SignupUserAsync(userRequest);
+
+            return Ok(authUser);
         }
 
         /// <summary>
         /// Logs in a user and returns a JWT token for authentication
+        /// Temporary to be replaced by SPA
         /// </summary>
         /// <param name="request"></param>
         /// <returns>JWT token for authentication</returns>
         /// <exception cref="NotImplementedException"></exception>
         [HttpPost("login")]
-        public IActionResult Login(LoginRequest request)
+        public async Task<IActionResult> Login(LoginRequest request)
         {
-            throw new NotImplementedException();
+            var tokenRequest = new ResourceOwnerTokenRequest
+            {
+                ClientId = _authOptions.Value.ClientId,
+                ClientSecret = _authOptions.Value.ClientSecret,
+                Scope = "openid profile",
+                Realm = _authOptions.Value.Connection,
+                Username = request.Username, 
+                Password = request.Password
+            };
+
+            var tokenResponse = await _authClient.GetTokenAsync(tokenRequest);
+
+            return Ok(new { tokenResponse.AccessToken });
         }
     }
 }
